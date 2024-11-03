8d25e562006a711f42a58c72b20a939802fccb1c
minhtam900 <dieptu900@gmail.com>
2024-11-03 18:25:39 +0700

    admin

--- src/app/home/component/home/home.component.ts
+++ src/app/home/component/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  showScrollButton: boolean = false;
+
+  @HostListener('window:scroll', [])
+  
+  onWindowScroll() {
+    this.showScrollButton = window.scrollY > 200;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); 
+  }
   constructor() { }
 
   ngOnInit(): void {
